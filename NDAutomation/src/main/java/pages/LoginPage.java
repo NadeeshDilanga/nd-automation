e02cca48cea5cce1a39afdbeecaf91d360ff804e
Nadeesh <nadeesh@ecyber.com>
2023-01-27 12:32:13 +0530

    Waits class added

--- NDAutomation/src/main/java/pages/LoginPage.java
+++ NDAutomation/src/main/java/pages/LoginPage.java
@@ -1,12 +1,3 @@
-/*
- *  Copyright 2023 Nadeesh Dilanga.
- *  All rights reserved. Reproduction or transmission in whole or in part,
- *  in any form or by any means, electronic, mechanical or otherwise, is prohibited
- *  without the prior written consent of the copyright owner.
- *  Author: Nadeesh Dilanga
- *  Date: 12/25/2022
- */
-
 package pages;
 
 import org.openqa.selenium.By;
